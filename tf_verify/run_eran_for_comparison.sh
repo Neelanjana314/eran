6d754ff8b97d050fa7b7ca6a27270863d7152150
Neelanjana Pal <38669616+Neelanjana314@users.noreply.github.com>
2021-04-25 21:03:53 -0500

    relocate comparison script to

--- tf_verify/run_eran_for_comparison.sh
+++ tf_verify/run_eran_for_comparison.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd eran/tf_verify 
+cd ../tf_verify 
 
 python3 . --netname ../CAV2021/mnist_0.1.onnx --epsilon 0.05 --mean 0.1307 --std 0.3081 --domain deeppoly --dataset mnist
 python3 . --netname ../CAV2021/mnist_0.1.onnx --epsilon 0.1 --mean 0.1307 --std 0.3081 --domain deeppoly --dataset mnist
